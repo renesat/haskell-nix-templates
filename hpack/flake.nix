8412d06e0f2ca9ebfa77578aba93522075f352c5
renesat <smol.ivan97@gmail.com>
2023-06-20 22:35:02 +0200

    Mode template to haskell-flake

--- hpack/flake.nix
+++ hpack/flake.nix
@@ -1,84 +1,97 @@
 {
-
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+    flake-root.url = "github:srid/flake-root";
+    mission-control.url = "github:Platonic-Systems/mission-control";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [
-      "aarch64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-      "x86_64-linux"
-    ] (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.haskell-flake.flakeModule
+        inputs.treefmt-nix.flakeModule
+        inputs.flake-root.flakeModule
+        inputs.mission-control.flakeModule
+      ];
+      systems = [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      perSystem = {
+        self',
+        system,
+        lib,
+        config,
+        pkgs,
+        ...
+      }: {
+        haskellProjects.default = {
+          devShell = {
+            # TODO: Remove this after https://github.com/numtide/treefmt-nix/issues/65
+            tools = hp:
+              {
+                hpack = hp.hpack;
+                treefmt = config.treefmt.build.wrapper;
+              }
+              // config.treefmt.build.programs;
+            hlsCheck.enable = false;
+          };
+          autoWire = ["packages" "apps" "checks"];
+        };
 
-        runInputs = [ ];
+        treefmt.config = {
+          inherit (config.flake-root) projectRootFile;
+          package = pkgs.treefmt;
 
-        # For adding tools, depends and other stuff use pkgs.haskell.lib.*
-        hello-world = pkgs.haskellPackages.developPackage {
-          root = self;
+          programs.ormolu.enable = true;
+          programs.ormolu.package = pkgs.haskellPackages.fourmolu_0_12_0_0;
+          programs.alejandra.enable = true;
+          programs.hlint.enable = true;
+        };
+
+        mission-control.scripts = {
+          repl = {
+            description = "Start the cabal repl";
+            exec = ''
+              cabal repl "$@"
+            '';
+            category = "Dev Tools";
+          };
+          fmt = {
+            description = "Format the source tree";
+            exec = config.treefmt.build.wrapper;
+            category = "Dev Tools";
+          };
+          build = {
+            description = "Build all";
+            exec = ''
+              cabal build
+            '';
+            category = "Dev Tools";
+          };
+        };
+
+        packages.default = self'.packages.hello-world;
+        apps.default = self'.apps.hello-world-exe;
+
+        devShells.default = pkgs.mkShell {
           name = "hello-world";
-          cabal2nixOptions = "--hpack";
+          inputsFrom = [
+            config.haskellProjects.default.outputs.devShell
+            config.flake-root.devShell
+            config.mission-control.devShell
+          ];
         };
-
-        stack-nix-integration = pkgs.writeText "stack-nix-integration.nix" ''
-          {ghc}:
-          let
-            pkgs = import ${pkgs.path} {};
-          in pkgs.haskell.lib.buildStackProject {
-            inherit ghc;
-            name = "hello-world";
-            buildInputs = with pkgs; [
-            ];
-          }
-        '';
-
-        # Source: https://www.tweag.io/blog/2022-06-02-haskell-stack-nix-shell/
-        stack-wrapped = pkgs.symlinkJoin {
-          name = "stack";
-          paths = [ pkgs.stack ];
-          buildInputs = [ pkgs.makeWrapper ];
-          postBuild = ''
-            wrapProgram $out/bin/stack \
-              --add-flags "\
-                --nix \
-                --no-nix-pure \
-                --nix-shell-file="${stack-nix-integration}"\
-              "
-          '';
-        };
-
-      in {
-        packages = {
-          inherit hello-world;
-          default = hello-world;
-        };
-        apps = {
-          hello-world = {
-            type = "app";
-            program = "${hello-world}/bin/hello-world-exe";
-          };
-          defalut = self.apps."${system}".hello-world;
-        };
-        devShells = {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs;
-              [
-                # Slack
-                stack-wrapped
-
-                # Tools
-                haskellPackages.hasktags
-                haskellPackages.fourmolu
-                haskell-language-server
-                nixfmt
-
-              ] ++ runInputs;
-            NIX_PATH = "nixpkgs=" + pkgs.path;
-          };
-        };
-      });
+      };
+    };
 }
